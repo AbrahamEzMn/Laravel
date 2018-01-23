88bfc8dd471fc58cfe7ddb9ba333ad39ea66f204
Abraham Esparza <abraham.esparza.m010@gmail.com>
2018-01-22 20:19:30 -0600

    Creacion de Registro controller

--- ApiRest/routes/api.php
+++ ApiRest/routes/api.php
@@ -17,6 +17,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * Ruta de prueba.
+ * 
+ * @url GET /ApiRest/public/api/foo
+ */
 Route::get('foo', function () {
     return 'Hello World';
 });
+
+/**
+ * Ruta para el manejo de resgistros.
+ * 
+ * @url GET /ApiRest/public/api/registro -- Lista de registros.
+ * @url GET /ApiRest/public/api/resgitro/{id} -- Retorna un registro por su Id.
+ * @url POST /ApiRest/public/api/registro -- Guarda un registro.
+ * @url UPDATE|PATCH /ApiRest/public/api/registro/{id} -- Modifica el registro por su Id.
+ * @url DELETE /ApiRest/public/api/registro/{id} -- Elimina el registro por el Id.
+ */
+Route::resource('registro', 'RegistroController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
